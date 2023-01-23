df090fb869bb6052fc883c97e2ba1e99ea773eed
@reroes <reroes799@gmail.com>
2023-01-23 17:16:34 -0500

    usando pymongo

--- agregar_informacion.py
+++ agregar_informacion.py
@@ -8,4 +8,22 @@ from enlace_base import client
 
 db = client.ejemploMongo001
 coleccion = db.autores
-print(coleccion)
+
+# conjunto de datos a guardar en la colección
+# importante, aquí se usa la estructura de Python denominada diccionario
+# proceso que agrega un solo documento
+data_01 = {"nombre": "Luis", "apellido": "Valencia",
+"nacionalidad":"ecuatoriana", "numero_publicaciones": 100}
+
+# coleccion.insert_one(data_01)
+
+# proceso que agrega una lista de documentos
+
+lista = [
+{"nombre": "José", "apellido": "Medina", "nacionalidad":"ecuatoriana",
+"numero_publicaciones": 90},
+{"nombre": "María", "apellido": "Velez", "nacionalidad":"peruana",
+"numero_publicaciones": 80}
+]
+
+coleccion.insert_many(lista)
